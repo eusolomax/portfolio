a79477ef5c2bbb8b11aebcb829862696c0a3578d
= <=>
2022-10-13 18:53:53 -0300

    first commit

---
+++ src/ROADMAP.md
@@ -0,0 +1,9 @@
+🔴 Cria tela inicial com minha imagem e com o texto:
+Olá, me chamo Victor.
+
+sou
+Desenvolvedor
+Frontend.
+
+E por aqui você poderá ver meus trabalhos e me conhecer melhor
+--------------------------------------------------------------------------------------------
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './css/land1.component.css',
+    './css/land2.component.css'
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'victor-portfolio';
+  sideIcon: any = "<\\>"
+
+  homeBackgroundVideo: any = document.getElementsByClassName("home-land1-background-video")
+  whoBackgroundVideo: any = document.getElementsByClassName("who-land2-background-video")
+
+  ngOnInit() {
+    this.homeBackgroundVideo.play()
+    this.whoBackgroundVideo.play()
+  }
 }
