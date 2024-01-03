189abbcf5463ab3ca493b6efb4bd34c43d72fa1d
Victor Capitelli <victor.capitelli@scicrop.com>
2024-01-03 00:12:41 -0300

    remove: useless component;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HeroSectionComponent
   ],
   imports: [
